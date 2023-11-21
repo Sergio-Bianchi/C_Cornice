7d3fc0c1ae011ec961128d605eddb37b352a4699
Sergio-Bianchi <sergio.bianchi.2007@calvino.edu.it>
2023-11-21 09:30:14 +0100

    A parte il fatto che non ho voglia di commentare tutto, l'idea adesso sarebbe creare una funzione da importarmi nei progettini che stampi sempre un menù identico tipo con input
    Titolo:
    Autore:
    Data:
    Desccrizione:

--- main.c
+++ main.c
@@ -1,47 +1,75 @@
 #include <stdio.h>
 
+
+/*
+ * Semplice programma per scrivere una cornice di asterischi in C
+ * ! I caratteri devono essere monospaced per funzionare
+ *
+ * Author: Sergio Bianchi
+ * Date:
+ *
+ * */
+
+
+
+void stampaOrizzontale(int width);
+
+void stampaVerticale(int width, int height);
+
+void stampaCornice(int width, int height);
+
+
 int main() {
 
     // Dichiarazione variabili
     int larghezza = 0;
     int altezza = 0;
-    int i;
-    int j;
 
 
     // Input dati
     printf("Inserisci la larghezza: ");
-    scanf("%d",&larghezza);
+    scanf("%d", &larghezza);
     printf("Inserisci l'altezza: ");
-    scanf("%d",&altezza);
+    scanf("%d", &altezza);
 
+    // Chiama la funzione per stampare la cornice
+    stampaCornice(larghezza, altezza);
+
+    return 0;
+}
+
+
+void stampaOrizzontale(int width) {
 
     // Scrivi asterischi x la larghezza
-    for (i = 0; i < larghezza; i++) printf("*");
 
+    for (int i = 0; i < width; i++) printf("*");
     // Vai a capo
+
     printf("\n");
 
+
+}
+
+void stampaVerticale(int width, int height) {
     // ...per quanta altezza -2 (lato sopra e lato sotto)
-    for (i = 0; i < (altezza - 2); i++) {
+    for (int i = 0; i < (height - 2); i++) {
 
         // Scrivi asterisco
         printf("*");
 
         // Inserisci spazi per la larghezza -2 (i due lati)
-        for (j = 0; j < (larghezza - 2); j++) printf(" ");
+        for (int j = 0; j < (width - 2); j++) printf(" ");
 
         // Asterisco e vai a capo per scrivere la prossima linea
         printf("*\n");
 
     }
+}
 
-    // Scrivi asterischi x la larghezza
-    for (i = 0; i < larghezza; i++) printf("*");
+void stampaCornice(int width, int height) {
+    stampaOrizzontale(width);
+    stampaVerticale(width, height);
+    stampaOrizzontale(width);
 
-    // Vai a capo
-    printf("\n");
-
-
-    return 0;
 }
