b93b2e2cc1179b70217902e5a60e48bb4cb9e60e
Sergio-Bianchi <sergio.bianchi.2007@calvino.edu.it>
2023-11-22 08:54:32 +0100

    Devo iniziare a scrivere dei commenti prima che il progetto degeneri e non ci capisca più nulla. Comunque sta procedendo bene

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
  *
  * */
 
+char titolo[] = "Esercitazione: C_Menu";
+char author[] = "Sergio Bianchi";
+int spaceMultiplier = 4;
 
 
 void stampaOrizzontale(int width);
@@ -27,13 +30,20 @@ int main() {
 
 
     // Input dati
-    printf("Inserisci la larghezza: ");
+    /*printf("Inserisci la larghezza: ");
     scanf("%d", &larghezza);
     printf("Inserisci l'altezza: ");
-    scanf("%d", &altezza);
+    scanf("%d", &altezza);*/
+
+
+
+
+    int larghezzaWIP = sizeof(titolo) / sizeof(titolo[0]) + 2 + 1 * spaceMultiplier;
+
+    //printf("%d \n", larghezzaWIP);
 
     // Chiama la funzione per stampare la cornice
-    stampaCornice(larghezza, altezza);
+    stampaCornice(larghezzaWIP, 6);
 
     return 0;
 }
@@ -43,7 +53,7 @@ void stampaOrizzontale(int width) {
 
     // Scrivi asterischi x la larghezza
 
-    for (int i = 0; i < width; i++) printf("=");
+    for (int i = 0; i < width - 1; i++) printf("═");
     // Vai a capo
 
     printf("\n");
@@ -52,22 +62,42 @@ void stampaOrizzontale(int width) {
 }
 
 void stampaVerticale(int width, int height) {
+
     // ...per quanta altezza -2 (lato sopra e lato sotto)
     for (int i = 0; i < (height - 2); i++) {
 
         // Scrivi asterisco
-        printf("|");
+        printf("║");
 
         // Inserisci spazi per la larghezza -2 (i due lati)
-        for (int j = 0; j < (width - 2); j++) printf(" ");
+        if (i == 1) {
+            for (int j = 0; j < (spaceMultiplier / 2); j++) printf(" ");
+            printf("%s", titolo);
+            for (int j = 0; j < (spaceMultiplier / 2); j++) printf(" ");
+
+        } else if (i==2) {
+            int authorSpace = width-2-sizeof(author)/sizeof(author[0]);
+            for (int j = 0; j < authorSpace/2; j++) printf(" ");
+            printf("%s", author);
+            for (int j = 0; j < authorSpace/2; j++) printf(" ");
+            if (authorSpace%2) printf(" ");
+        }
+
+        else {
+            for (int j = 0; j < (width - 3); j++) printf(" ");
+        }
+
 
         // Asterisco e vai a capo per scrivere la prossima linea
-        printf("|\n");
+        printf("║\n");
 
     }
 }
 
 void stampaCornice(int width, int height) {
+    printf("Width: %d ; Height: %d\n", width, height);
+
+
     stampaOrizzontale(width);
     stampaVerticale(width, height);
     stampaOrizzontale(width);
